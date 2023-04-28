443174b602231997e04afa11638ca432522d6bcc
KevinZea <kevinstevenzeasuarez@gmail.com>
2023-04-28 16:15:16 -0500

    add contein new

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import ambulanciaDos from './img/ambulancia2.jpeg'
 import ambulanciaTres from './img/ambulancia3.jpeg'
 import ambulanciaCuatro from './img/ambulancia4.jpeg'
 import ambulanciaCinco from './img/ambulancia5.jpeg'
+import ambulanciaSeis from './img/ambulancia6.jpeg'
+import ambulanciaSiete from './img/ambulancia7.jpeg'
+import ambulanciaOcho from './img/ambulancia8.jpeg'
 
 import eventoUno from './img/evento1.jpeg'
 import eventoDos from './img/evento2.jpeg'
@@ -11,6 +14,11 @@ import eventoTres from './img/evento3.jpeg'
 import eventoCuatro from './img/evento4.jpeg'
 
 import videoUno from './img/videos/videoUno.mp4'
+import videoDos from './img/videos/video2.mp4'
+import videoTres from './img/videos/video3.mp4'
+import videoCuatro from './img/videos/video4.mp4'
+
+import videoAmbulancia from './img/videos/AmbulanciaEV.mp4'
 
 import iconoS from './img/iconoS.png'
 
@@ -164,6 +172,15 @@ function App() {
             <div class="carousel-item">
               <img src={ambulanciaCinco} class="d-block w-100" alt="..." />
             </div>
+            <div class="carousel-item">
+              <img src={ambulanciaSeis} class="d-block w-100" alt="..." />
+            </div>
+            <div class="carousel-item">
+              <img src={ambulanciaSiete} class="d-block w-100" alt="..." />
+            </div>
+            <div class="carousel-item">
+              <img src={ambulanciaOcho} class="d-block w-100" alt="..." />
+            </div>
           </div>
           <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -174,6 +191,33 @@ function App() {
             <span class="visually-hidden">Next</span>
           </button>
         </div>
+      </div>
+      <br></br>
+      <br></br>
+      <div className='videoAmbulancia'>
+        <video controls name="media">
+          <source src={videoAmbulancia} type="video/mp4"></source>
+        </video>
+        <div className='infoVideo'>
+          <h2>Nuestras Ambulancias por dentro:</h2>
+          <p>En este breve video se muestra como es una de nuestras ambulancias
+            puliendose para estar lista en las mejores condiciones para ser usada,
+            tambien se puede apreciar de sus debidas especificacion y con los mas
+            altos estandares de atención al paciente.
+          </p>
+          <div className='contactWhatsapp'>
+            <a href='https://api.whatsapp.com/send?phone=573103391442' target='_blank'>Escribenos
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+                <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className='videoPresentacionAmbulancia'>
+        <video controls name="media">
+          <source src={videoTres} type="video/mp4"></source>
+        </video>
       </div>
 
       <br></br>
@@ -189,38 +233,65 @@ function App() {
           <img src={eventoDos}></img>
           <img src={eventoTres}></img>
           <img src={eventoCuatro}></img>
+          <img src={ambulanciaSiete}></img>
+          <img src={ambulanciaOcho}></img>
         </div>
         <br></br>
         <br></br>
-        <div className='textEventos'>
-          <h1>Con Videos:</h1>
-        </div>
-        <div className="videos">
-          <video controls name="media">
-            <source src={videoUno} type="video/mp4"></source>
-          </video>
+        <div className='videosEventos'>
+          <div className='textEventos'>
+            <h1>Videos en eventos que hemos participado</h1>
+          </div>
+          <div className="videos">
+            <video controls name="media">
+              <source src={videoUno} type="video/mp4"></source>
+            </video>
+            <video controls name="media">
+              <source src={videoDos} type="video/mp4"></source>
+            </video>
+            <video controls name="media">
+              <source src={videoCuatro} type="video/mp4"></source>
+            </video>
+          </div>
         </div>
       </div>
 
       <br></br>
       <div className='contact'>
-        <div className='contactWhats'>
-          <a href='https://api.whatsapp.com/send?phone=573103391442' target='_blank'>Escribenos
-            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
-              <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
+        <div className='infoContact'>
+          <h4>Telefono: +57 3103391442
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-telephone-outbound" viewBox="0 0 16 16">
+              <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM11 .5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V1.707l-4.146 4.147a.5.5 0 0 1-.708-.708L14.293 1H11.5a.5.5 0 0 1-.5-.5z" />
             </svg>
-          </a>
-        </div>
-        <br></br>
-        <br></br>
-        <div className='correo'>
-          <a href='https://mail.google.com/mail/u/0/#inbox?compose=CllgCJlLWjLgJKXTgpFVHQRplSczFzXsPzhhlbcNcjVCcBPkcpMTThzJgFcCkvWjxjTxpKcMVqB' target='_blank'>
-            Correo electronico
-            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-envelope-at" viewBox="0 0 16 16">
+          </h4>
+          <br></br>
+          <h4>
+            Correo Electronico: ambulanciasparticulares1975@gmail.com
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-envelope-at" viewBox="0 0 16 16">
               <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2H2Zm3.708 6.208L1 11.105V5.383l4.708 2.825ZM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2-7-4.2Z" />
               <path d="M14.247 14.269c1.01 0 1.587-.857 1.587-2.025v-.21C15.834 10.43 14.64 9 12.52 9h-.035C10.42 9 9 10.36 9 12.432v.214C9 14.82 10.438 16 12.358 16h.044c.594 0 1.018-.074 1.237-.175v-.73c-.245.11-.673.18-1.18.18h-.044c-1.334 0-2.571-.788-2.571-2.655v-.157c0-1.657 1.058-2.724 2.64-2.724h.04c1.535 0 2.484 1.05 2.484 2.326v.118c0 .975-.324 1.39-.639 1.39-.232 0-.41-.148-.41-.42v-2.19h-.906v.569h-.03c-.084-.298-.368-.63-.954-.63-.778 0-1.259.555-1.259 1.4v.528c0 .892.49 1.434 1.26 1.434.471 0 .896-.227 1.014-.643h.043c.118.42.617.648 1.12.648Zm-2.453-1.588v-.227c0-.546.227-.791.573-.791.297 0 .572.192.572.708v.367c0 .573-.253.744-.564.744-.354 0-.581-.215-.581-.8Z" />
             </svg>
-          </a>
+          </h4>
+        </div>
+        <div className='contactos'>
+          <div className='contactWhats'>
+            <a href='https://api.whatsapp.com/send?phone=573103391442' target='_blank'>Escribenos
+              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+                <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
+              </svg>
+            </a>
+          </div>
+          <br></br>
+          <br></br>
+          <div className='correo'>
+            <a href='https://mail.google.com/mail/u/0/#inbox?compose=CllgCJlLWjLgJKXTgpFVHQRplSczFzXsPzhhlbcNcjVCcBPkcpMTThzJgFcCkvWjxjTxpKcMVqB' target='_blank'>
+              Correo electronico
+              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-envelope-at" viewBox="0 0 16 16">
+                <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2H2Zm3.708 6.208L1 11.105V5.383l4.708 2.825ZM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2-7-4.2Z" />
+                <path d="M14.247 14.269c1.01 0 1.587-.857 1.587-2.025v-.21C15.834 10.43 14.64 9 12.52 9h-.035C10.42 9 9 10.36 9 12.432v.214C9 14.82 10.438 16 12.358 16h.044c.594 0 1.018-.074 1.237-.175v-.73c-.245.11-.673.18-1.18.18h-.044c-1.334 0-2.571-.788-2.571-2.655v-.157c0-1.657 1.058-2.724 2.64-2.724h.04c1.535 0 2.484 1.05 2.484 2.326v.118c0 .975-.324 1.39-.639 1.39-.232 0-.41-.148-.41-.42v-2.19h-.906v.569h-.03c-.084-.298-.368-.63-.954-.63-.778 0-1.259.555-1.259 1.4v.528c0 .892.49 1.434 1.26 1.434.471 0 .896-.227 1.014-.643h.043c.118.42.617.648 1.12.648Zm-2.453-1.588v-.227c0-.546.227-.791.573-.791.297 0 .572.192.572.708v.367c0 .573-.253.744-.564.744-.354 0-.581-.215-.581-.8Z" />
+              </svg>
+            </a>
+          </div>
         </div>
       </div>
 
